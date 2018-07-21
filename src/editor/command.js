22a8a80a05b090cc83100a997f56087d10506759
Ayhan Akilli <info@ayhan.biz>
2018-07-21 11:50:53 +0200

    Start simple

--- src/editor/command.js
+++ src/editor/command.js
@@ -3,38 +3,6 @@
  */
 class Command {
     /**
-     * Initializes a new editor command with given ID and name
-     *
-     * @param {string} id
-     * @param {string} name
-     */
-    constructor(id, name) {
-        if (typeof id !== 'string' || id.length <= 0) {
-            throw 'Invalid command id';
-        }
-
-        if (typeof name !== 'string' || name.length <= 0) {
-            throw 'Invalid command name';
-        }
-
-        /**
-         * Command ID
-         *
-         * @type {string}
-         * @readonly
-         */
-        this.id = id;
-
-        /**
-         * Command name
-         *
-         * @type {string}
-         * @readonly
-         */
-        this.name = name;
-    }
-
-    /**
      * Execute command
      */
     execute() {}
