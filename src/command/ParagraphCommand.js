d34f53b3a45895aa93c4776aa75afdf62abd01e8
Ayhan Akilli <info@ayhan.biz>
2018-07-24 16:14:13 +0200

    Use formatblock instead of insertparagraph

--- src/command/ParagraphCommand.js
+++ src/command/ParagraphCommand.js
@@ -8,6 +8,6 @@ export default class ParagraphCommand extends Command {
      * @inheritDoc
      */
     execute() {
-        this.editor.execute('insertparagraph');
+        this.editor.execute('formatblock', '<p>');
     }
 }
