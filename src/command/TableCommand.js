0a1487b943fd8d645966d5cd147ff5b8b989cda9
Ayhan Akilli <info@ayhan.biz>
2018-07-25 21:42:20 +0200

    Prepare dynamic table

--- src/command/TableCommand.js
+++ src/command/TableCommand.js
@@ -8,6 +8,21 @@ export default class TableCommand extends Command {
      * @inheritDoc
      */
     execute() {
-        this.editor.document.execCommand('inserthtml', false, '<table><thead><tr><th></th><th></th></tr></thead><tfoot><tr><td></td><td></td></tr></tfoot><tbody><tr><td></td><td></td></tr></tbody></table>');
+        const table = this.editor.document.createElement('table');
+
+        ['thead', 'tfoot', 'tbody'].forEach(part => {
+            const item = this.editor.document.createElement(part);
+            const tr = this.editor.document.createElement('tr');
+
+            table.appendChild(item);
+            item.appendChild(tr);
+
+            for (let i = 0; i < 2; ++i) {
+                let cell = this.editor.document.createElement(item.tagName === 'thead' ? 'th' : 'td');
+                tr.appendChild(cell);
+            }
+        });
+
+        this.editor.document.execCommand('inserthtml', false, table.outerHTML);
     }
 }
