d2b5d33c08eb03b72de73d4ef4e98275276325ed
Ayhan Akilli <info@ayhan.biz>
2018-07-29 01:20:10 +0200

    Register keyup listener to disable default spacebar behaviour in summary element

--- src/command/DetailsCommand.js
+++ src/command/DetailsCommand.js
@@ -7,6 +7,31 @@ export default class DetailsCommand extends Command {
     /**
      * @inheritDoc
      */
+    constructor(editor) {
+        super(editor);
+
+        const mutation = new MutationObserver(ev => {
+            ev.forEach(item => {
+                item.addedNodes.forEach(node => {
+                    let summary;
+
+                    if (node.tagName.toLowerCase() === 'details' && !!(summary = node.querySelector('summary'))) {
+                        summary.addEventListener('keyup', e => {
+                            if (e.keyCode === 32) {
+                                e.preventDefault();
+                                this.editor.document.execCommand('inserttext', false, ' ');
+                            }
+                        });
+                    }
+                });
+            });
+        });
+        mutation.observe(this.editor.element, {childList: true});
+    }
+
+    /**
+     * @inheritDoc
+     */
     execute() {
         const details = this.editor.document.createElement('details');
         const summary = this.editor.document.createElement('summary');
