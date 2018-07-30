8498c3e6b36a5fb5a7bc1eda407e594767606be3
Ayhan Akilli <info@ayhan.biz>
2018-07-30 11:36:39 +0200

    Merge subheading back to heading

--- src/command/HeadingCommand.js
+++ src/command/HeadingCommand.js
@@ -1,14 +1,41 @@
 import Command from './Command.js';
 
 /**
+ * Allowed heading tags
+ *
+ * @type {string[]}
+ */
+const tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+
+/**
  * Heading Command
  */
 export default class HeadingCommand extends Command {
     /**
+     * Initializes a new editor command and registers heading tag
+     *
+     * @param {Editor} editor
+     * @param {string} tag
+     */
+    constructor(editor, tag) {
+        super(editor);
+
+        if (!tag || !tags.includes(tag) || !this.editor.getTag(tag)) {
+            throw 'Invalid heading element';
+        }
+
+        /**
+         * @type {string}
+         * @readonly
+         */
+        this.tag = tag;
+    }
+
+    /**
      * @inheritDoc
      */
     execute() {
-        const h = document.createElement('h2');
+        const h = document.createElement(this.tag);
         h.innerText = 'Heading';
         this.editor.insert(h);
     }
