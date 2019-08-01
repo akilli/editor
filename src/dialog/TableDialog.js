a608e9694b6881f13f70ff3202fb519439878c9a
Ayhan Akilli <info@ayhan.biz>
2019-08-01 21:58:04 +0200

    Prefix dialog ids

--- src/dialog/TableDialog.js
+++ src/dialog/TableDialog.js
@@ -11,12 +11,12 @@ export default class TableDialog extends Dialog {
         super(editor, save, `
             <legend>Table</legend>
             <div data-attr="rows" data-required>
-                <label for="rows">Rows</label>
-                <input id="rows" name="rows" type="number" value="1" required="required" min="1" />
+                <label for="editor-rows">Rows</label>
+                <input id="editor-rows" name="rows" type="number" value="1" required="required" min="1" />
             </div>
             <div data-attr="cols" data-required>
-                <label for="cols">Cols</label>
-                <input id="cols" name="cols" type="number" value="1" required="required" min="1" />
+                <label for="editor-cols">Cols</label>
+                <input id="editor-cols" name="cols" type="number" value="1" required="required" min="1" />
             </div>
         `);
     }
