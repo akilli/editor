ea1b57757a91478cdedaa497bdafebee7afff395
Ayhan Akilli <info@ayhan.biz>
2019-08-19 16:00:50 +0200

    Reorder tags

--- cfg/tag.js
+++ cfg/tag.js
@@ -4,30 +4,28 @@
  * @type {Object[]}
  */
 export default [
+    // Root
     {name: 'root', children: ['details', 'figure', 'heading', 'list', 'p', 'section']},
-    {name: 'a', group: 'text', attributes: ['href']},
-    {name: 'audio', group: 'media', attributes: ['controls', 'height', 'src', 'width'], empty: true},
-    {name: 'blockquote', children: ['p']},
-    {name: 'br', empty: true},
-    {name: 'cite', group: 'text'},
-    {name: 'details', children: ['figure', 'list', 'p', 'summary']},
-    {name: 'dfn', group: 'text'},
-    {name: 'figcaption', children: ['text'], editable: true, enter: 'p'},
-    {name: 'figure', attributes: ['class'], children: ['blockquote', 'figcaption', 'media', 'table']},
+    // Paragraph
+    {name: 'p', children: ['br', 'text'], editable: true, enter: 'p'},
+    // Heading
     {name: 'h2', group: 'heading', editable: true, enter: 'p'},
     {name: 'h3', group: 'heading', editable: true, enter: 'p'},
-    {name: 'i', group: 'text'},
+    // List
+    {name: 'ol', group: 'list', children: ['li']},
+    {name: 'ul', group: 'list', children: ['li']},
+    {name: 'li', children: ['br', 'text'], editable: true, enter: 'li'},
+    // Figure
+    {name: 'figure', attributes: ['class'], children: ['blockquote', 'figcaption', 'media', 'table']},
+    {name: 'figcaption', children: ['text'], editable: true, enter: 'p'},
+    // Blockquote
+    {name: 'blockquote', children: ['p']},
+    // Media
+    {name: 'audio', group: 'media', attributes: ['controls', 'height', 'src', 'width'], empty: true},
     {name: 'iframe', group: 'media', attributes: ['allowfullscreen', 'height', 'src', 'width'], empty: true},
     {name: 'img', group: 'media', attributes: ['alt', 'height', 'src', 'width'], empty: true},
-    {name: 'li', children: ['br', 'text'], editable: true, enter: 'li'},
-    {name: 'kbd', group: 'text'},
-    {name: 'mark', group: 'text'},
-    {name: 'ol', group: 'list', children: ['li']},
-    {name: 'p', children: ['br', 'text'], editable: true, enter: 'p'},
-    {name: 'q', group: 'text'},
-    {name: 'section', attributes: ['class'], children: ['details', 'figure', 'list', 'heading', 'p']},
-    {name: 'strong', group: 'text'},
-    {name: 'summary', editable: true, enter: 'p'},
+    {name: 'video', group: 'media', attributes: ['controls', 'height', 'src', 'width'], empty: true},
+    // Table
     {name: 'table', children: ['tablesection']},
     {name: 'tbody', group: 'tablesection', children: ['tr']},
     {name: 'td', group: 'tablecell', children: ['br', 'text'], editable: true},
@@ -35,6 +33,20 @@ export default [
     {name: 'th', group: 'tablecell', children: ['br', 'text'], editable: true},
     {name: 'thead', group: 'tablesection', children: ['tr']},
     {name: 'tr', children: ['tablecell']},
-    {name: 'ul', group: 'list', children: ['li']},
-    {name: 'video', group: 'media', attributes: ['controls', 'height', 'src', 'width'], empty: true}
+    // Details
+    {name: 'details', children: ['figure', 'list', 'p', 'summary']},
+    {name: 'summary', editable: true, enter: 'p'},
+    // Section
+    {name: 'section', attributes: ['class'], children: ['details', 'figure', 'list', 'heading', 'p']},
+    // Text
+    {name: 'strong', group: 'text'},
+    {name: 'i', group: 'text'},
+    {name: 'dfn', group: 'text'},
+    {name: 'kbd', group: 'text'},
+    {name: 'mark', group: 'text'},
+    {name: 'q', group: 'text'},
+    {name: 'cite', group: 'text'},
+    {name: 'a', group: 'text', attributes: ['href']},
+    // Break
+    {name: 'br', empty: true},
 ]
