5e58ab9e587f2ca2a16f93e5d455cc2a69c16b8a
Ayhan Akilli <info@ayhan.biz>
2019-08-22 17:00:14 +0200

    Remove TableObserver for now

--- cfg/observer.js
+++ cfg/observer.js
@@ -4,7 +4,6 @@ import DraggableObserver from '../src/observer/DraggableObserver.js';
 import EditableObserver from '../src/observer/EditableObserver.js';
 import FigureObserver from '../src/observer/FigureObserver.js';
 import SummaryObserver from '../src/observer/SummaryObserver.js';
-import TableObserver from '../src/observer/TableObserver.js';
 
 /**
  * Observer configuration
@@ -17,6 +16,5 @@ export default [
     DisableDragObserver,
     DraggableObserver,
     FigureObserver,
-    TableObserver,
     DeleteObserver,
 ]
