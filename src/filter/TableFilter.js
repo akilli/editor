553e3294f3f50d09be518ec596542b8df349f851
Ayhan Akilli <info@ayhan.biz>
2019-08-22 16:51:46 +0200

    Fix TableFilter

--- src/filter/TableFilter.js
+++ src/filter/TableFilter.js
@@ -8,14 +8,10 @@ export default class TableFilter extends Filter {
      * @inheritDoc
      */
     filter(parent, forceRoot = false) {
-        ['tbody', 'thead', 'tfoot', 'tr'].forEach(item => parent.querySelectorAll(item).forEach(node => {
-            if (!node.querySelector('th:not(:empty), td:not(:empty)')) {
-                if (item === 'tbody') {
-                    node.parentElement.parentElement.removeChild(node.parentElement);
-                } else {
-                    node.parentElement.removeChild(node);
-                }
+        if (parent instanceof HTMLTableRowElement && !parent.querySelector('th:not(:empty), td:not(:empty)')) {
+            while (parent.firstChild) {
+                parent.removeChild(parent.firstChild);
             }
-        }));
+        }
     }
 }
