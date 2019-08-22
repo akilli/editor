a647e91dfc4bab8e13dac7ab07a89f8012d672dd
Ayhan Akilli <info@ayhan.biz>
2019-08-22 23:30:42 +0200

    Extract event listener

--- src/observer/WidgetObserver.js
+++ src/observer/WidgetObserver.js
@@ -11,26 +11,35 @@ export default class WidgetObserver extends Observer {
         ev.forEach(item => item.addedNodes.forEach(node => {
             if (node instanceof HTMLElement && node.parentElement.isSameNode(this.editor.content)) {
                 node.tabIndex = 0;
-                node.addEventListener('keyup', ev => {
-                    if (this.editor.document.activeElement.isSameNode(node)) {
-                        if (ev.key === 'Delete') {
-                            node.parentElement.removeChild(node);
-                            ev.preventDefault();
-                            ev.cancelBubble = true;
-                        } else if (ev.key === 'ArrowUp' && node.previousElementSibling) {
-                            node.previousElementSibling.insertAdjacentHTML('beforebegin', node.outerHTML);
-                            node.parentElement.removeChild(node);
-                            ev.preventDefault();
-                            ev.cancelBubble = true;
-                        } else if (ev.key === 'ArrowDown' && node.nextElementSibling) {
-                            node.nextElementSibling.insertAdjacentHTML('afterend', node.outerHTML);
-                            node.parentElement.removeChild(node);
-                            ev.preventDefault();
-                            ev.cancelBubble = true;
-                        }
-                    }
-                });
+                node.addEventListener('keyup', ev => this.onKeyUp(ev));
             }
         }));
+    }
+
+    /**
+     * Handles keyup events
+     *
+     * @param {KeyboardEvent} ev
+     */
+    onKeyUp(ev) {
+        const node = ev.target;
+
+        if (this.editor.document.activeElement.isSameNode(node)) {
+            if (ev.key === 'Delete') {
+                node.parentElement.removeChild(node);
+                ev.preventDefault();
+                ev.cancelBubble = true;
+            } else if (ev.key === 'ArrowUp' && node.previousElementSibling) {
+                node.previousElementSibling.insertAdjacentHTML('beforebegin', node.outerHTML);
+                node.parentElement.removeChild(node);
+                ev.preventDefault();
+                ev.cancelBubble = true;
+            } else if (ev.key === 'ArrowDown' && node.nextElementSibling) {
+                node.nextElementSibling.insertAdjacentHTML('afterend', node.outerHTML);
+                node.parentElement.removeChild(node);
+                ev.preventDefault();
+                ev.cancelBubble = true;
+            }
+        }
     }
 }
