cff5ff748b9540bcd7b187153151e05a3324a7e7
Ayhan Akilli <info@ayhan.biz>
2020-01-25 14:53:58 +0100

    Prepare initial release

--- demo/index.js
+++ demo/index.js
@@ -4,10 +4,10 @@ import Editor from '../src/Editor.js';
     document.addEventListener('DOMContentLoaded', () => {
         Editor.create(document.getElementById('rte'), {
             media: {
-                audio: '/demo-browser/media.html#audio',
-                iframe: '/demo-browser/media.html#iframe',
-                image: '/demo-browser/media.html#image',
-                video: '/demo-browser/media.html#video',
+                audio: 'https://akilli.github.io/demo-browser/media.html#audio',
+                iframe: 'https://akilli.github.io/demo-browser/media.html#iframe',
+                image: 'https://akilli.github.io/demo-browser/media.html#image',
+                video: 'https://akilli.github.io/demo-browser/media.html#video',
             },
         })
         .then(editor => document.getElementById('save').addEventListener('click', () => console.log(editor.getData())))
