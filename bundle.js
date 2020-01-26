96ca22e347b7f69a7b7807009118e114f17eb1b3
Ayhan Akilli <info@ayhan.biz>
2020-01-26 13:24:00 +0100

    Add css and font file to bundle

---
+++ bundle.js
@@ -0,0 +1,5 @@
+import Editor from './src/Editor.js';
+import './gui/editor.css';
+import './gui/editor.woff2';
+
+export default Editor;
--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,25 @@
+import css from 'rollup-plugin-css-porter';
+import url from '@rollup/plugin-url';
 import {terser} from 'rollup-plugin-terser';
 
 export default {
-    input: 'src/Editor.js',
+    input: 'bundle.js',
     output: {
         file: 'dist/editor.js',
         format: 'esm',
         preferConst: true,
         plugins: [terser()],
-    }
+    },
+    plugins: [
+        css({
+            minified: 'dist/editor.css',
+            raw: false,
+        }),
+        url({
+            destDir: 'dist',
+            fileName: '[name][extname]',
+            include: ['**/*.woff2'],
+            limit: 0,
+        })
+    ]
 };
