61a26108603316f88808612a9452fc461b1c4379
Ayhan Akilli <info@ayhan.biz>
2020-03-29 11:45:50 +0200

    Clean up

--- cfg/media.js
+++ cfg/media.js
@@ -15,21 +15,21 @@ export default {
         mime: [
             'audio/aac', 'audio/flac', 'audio/mp3', 'audio/mpeg', 'audio/mpeg3', 'audio/ogg', 'audio/wav', 'audio/wave', 'audio/webm',
             'audio/x-aac', 'audio/x-flac', 'audio/x-mp3', 'audio/x-mpeg', 'audio/x-mpeg3', 'audio/x-pn-wav', 'audio/x-wav'
-        ]
+        ],
     },
     iframe: {
         id: 'iframe',
         element: 'iframe',
-        mime: ['text/html']
+        mime: ['text/html'],
     },
     image: {
         id: 'image',
         element: 'img',
-        mime: ['image/gif', 'image/jpeg', 'image/png', 'image/svg+xml', 'image/webp']
+        mime: ['image/gif', 'image/jpeg', 'image/png', 'image/svg+xml', 'image/webp'],
     },
     video: {
         id: 'video',
         element: 'video',
-        mime: ['video/mp4', 'video/ogg', 'video/webm']
-    }
+        mime: ['video/mp4', 'video/ogg', 'video/webm'],
+    },
 }
