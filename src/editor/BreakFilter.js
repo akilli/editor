c2497b0ef75bc6c15574328fa581251884982773
Ayhan Akilli <info@ayhan.biz>
2020-04-11 14:17:30 +0200

    Refactor br filter

--- src/editor/BreakFilter.js
+++ src/editor/BreakFilter.js
@@ -8,14 +8,9 @@ export default class BreakFilter extends Filter {
      * @inheritDoc
      */
     filter(parent, forceRoot = false) {
-        parent.querySelectorAll(':scope > br').forEach(node => {
-            if (parent.firstChild.isSameNode(node) || node.previousSibling instanceof HTMLBRElement || parent.lastChild.isSameNode(node)) {
-                parent.removeChild(node);
-            }
-        });
-
-        if (parent.lastChild instanceof HTMLBRElement) {
-            parent.removeChild(parent.lastChild);
-        }
+        parent.innerHTML = parent.innerHTML
+            .replace(/^\s*(<br\ ?\/?>\s*)+/gi, '')
+            .replace(/\s*(<br\ ?\/?>\s*)+$/gi, '')
+            .replace(/\s*(<br\ ?\/?>\s*)+/gi, '<br>');
     }
 }
