4554f14cafa4b3158b8db85681adae23eb133bac
Ayhan Akilli <info@ayhan.biz>
2020-04-11 14:30:12 +0200

    Extend br filter to split paragraphs with multiple br elements into multiple paragraphs

--- src/editor/BreakFilter.js
+++ src/editor/BreakFilter.js
@@ -8,12 +8,12 @@ export default class BreakFilter extends Filter {
      * @inheritDoc
      */
     filter(parent, forceRoot = false) {
-        parent.innerHTML = parent.innerHTML.replace(/^\s*(<br\ ?\/?>\s*)+/gi, '').replace(/\s*(<br\ ?\/?>\s*)+$/gi, '');
+        parent.innerHTML = parent.innerHTML.replace(/^\s*(<br ?\/?>\s*)+/gi, '').replace(/\s*(<br ?\/?>\s*)+$/gi, '');
 
         if (parent instanceof HTMLParagraphElement) {
-            parent.outerHTML = parent.outerHTML.replace(/\s*(<br\ ?\/?>\s*)+/gi, '</p><p>');
+            parent.outerHTML = parent.outerHTML.replace(/\s*(<br ?\/?>\s*)+/gi, '</p><p>');
         } else{
-            parent.innerHTML = parent.innerHTML.replace(/\s*(<br\ ?\/?>\s*)+/gi, '<br>');
+            parent.innerHTML = parent.innerHTML.replace(/\s*(<br ?\/?>\s*)+/gi, '<br>');
         }
     }
 }
