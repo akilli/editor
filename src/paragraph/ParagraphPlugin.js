c5e6118c80aaa262821beb769176a0e6a27b059e
Ayhan Akilli <info@ayhan.biz>
2020-04-22 22:20:29 +0200

    Cleanup

--- src/paragraph/ParagraphPlugin.js
+++ src/paragraph/ParagraphPlugin.js
@@ -19,7 +19,8 @@ export default class ParagraphPlugin extends Plugin {
      * @inheritDoc
      */
     init() {
-        this.editor.tags.set(new Tag({name: 'p', group: 'paragraph', children: ['break', 'text'], editable: true, enter: 'p'}));
-        this.editor.commands.set(new Command(this.editor, this.name, 'p'));
+        const tagName = 'p';
+        this.editor.tags.set(new Tag({name: tagName, group: 'paragraph', children: ['break', 'text'], editable: true, enter: 'p'}));
+        this.editor.commands.set(new Command(this.editor, this.name, tagName));
     }
 }
