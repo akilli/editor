2c95fd7b23c49e03d92ee0bf35c7cc293c574053
Ayhan Akilli <info@ayhan.biz>
2020-05-01 11:39:03 +0200

    Add toolbar focus handling

--- src/base/ToolbarObserver.js
+++ src/base/ToolbarObserver.js
@@ -9,12 +9,45 @@ export default class ToolbarObserver extends Observer {
      */
     observe(ev) {
         ev.forEach(item => item.addedNodes.forEach(node => {
-            let cmd;
-            let command;
+            if (node instanceof HTMLElement) {
+                let cmd;
+                let command;
 
-            if (node instanceof HTMLElement && (cmd = node.getAttribute('data-cmd')) && (command = this.editor.commands.get(cmd))) {
-                node.addEventListener('click', () => command.execute());
+                if (node instanceof HTMLElement && (cmd = node.getAttribute('data-cmd')) && (command = this.editor.commands.get(cmd))) {
+                    node.addEventListener('click', () => command.execute());
+                }
+
+                if (this.editor.toolbar.isSameNode(node.parentElement)) {
+                    node.tabIndex = -1;
+                    this.keyboard(node);
+                }
             }
         }));
+    }
+
+    /**
+     * Handles keyboard events
+     *
+     * @private
+     *
+     * @param {HTMLElement} node
+     */
+    keyboard(node) {
+        node.addEventListener('keyup', ev => {
+            if (this.editor.document.activeElement.isSameNode(node) && ['ArrowLeft', 'ArrowRight', 'Home', 'End'].includes(ev.key)) {
+                if (ev.key === 'ArrowLeft') {
+                    node.previousElementSibling ? node.previousElementSibling.focus() : node.parentElement.lastElementChild.focus();
+                } else if (ev.key === 'ArrowRight') {
+                    node.nextElementSibling ? node.nextElementSibling.focus() : node.parentElement.firstElementChild.focus();
+                } else if (ev.key === 'Home') {
+                    node.parentElement.firstElementChild.focus();
+                } else if (ev.key === 'End') {
+                    node.parentElement.lastElementChild.focus();
+                }
+
+                ev.preventDefault();
+                ev.cancelBubble = true;
+            }
+        });
     }
 }
