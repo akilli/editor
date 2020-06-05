5b3b67f1033867ae3a65ce4abd8735f7d1cce211
Ayhan Akilli <info@ayhan.biz>
2020-06-05 15:50:11 +0200

    Handle keyboard events

--- src/base/NavigableObserver.js
+++ src/base/NavigableObserver.js
@@ -38,7 +38,7 @@ export default class NavigableObserver extends Observer {
      */
     keyboard(node) {
         node.addEventListener('keyup', ev => {
-            if (ev.target === node && !ev.ctrlKey && ['ArrowUp', 'ArrowDown', 'Home', 'End'].includes(ev.key)) {
+            if (ev.target === node && !ev.altKey && !ev.ctrlKey && !ev.shiftKey && ['ArrowUp', 'ArrowDown', 'Home', 'End'].includes(ev.key)) {
                 const prev = node.previousElementSibling;
                 const next = node.nextElementSibling;
                 const first = node.parentElement.firstElementChild;
