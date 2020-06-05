9e97ebcda0f421f091fe9579e8dda00c18c83264
Ayhan Akilli <info@ayhan.biz>
2020-06-05 08:37:15 +0200

    toggle :-@

--- src/fullscreen/FullscreenCommand.js
+++ src/fullscreen/FullscreenCommand.js
@@ -17,10 +17,6 @@ export default class FullscreenCommand extends Command {
      * @inheritDoc
      */
     execute() {
-        if (this.editor.element.classList.contains('editor-fullscreen')) {
-            this.editor.element.classList.remove('editor-fullscreen');
-        } else {
-            this.editor.element.classList.add('editor-fullscreen');
-        }
+        this.editor.element.classList.toggle('editor-fullscreen');
     }
 }
