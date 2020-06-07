31ca6a5d6fbe8df3366a7fec7e7e91b31101e187
Ayhan Akilli <info@ayhan.biz>
2020-06-07 17:24:40 +0200

    Avoid multiple registrations of the same listeners due to arrow function usage

--- src/table/TableObserver.js
+++ src/table/TableObserver.js
@@ -10,107 +10,119 @@ export default class TableObserver extends Observer {
     observe(records) {
         records.forEach(record => record.addedNodes.forEach(node => {
             if (node instanceof HTMLTableElement) {
-                this.init(node)
+                this.initTable(node)
+            } else if (node instanceof HTMLTableCellElement) {
+                this.initCell(node);
             } else if (node instanceof HTMLElement) {
-                node.querySelectorAll('table').forEach(item => this.init(item));
+                node.querySelectorAll('table').forEach(item => this.initTable(item));
+                node.querySelectorAll('td, th').forEach(item => this.initCell(item));
             }
         }));
     }
 
     /**
-     * Initializes table
+     * Initializes table element
      *
      * @private
-     * @param {HTMLTableElement} table
+     * @param {HTMLTableElement} node
      */
-    init(table) {
-        this.sections(table);
-        this.keyboard(table);
-    }
-
-    /**
-     * Creates missing table sections
-     *
-     * @private
-     * @param {HTMLTableElement} table
-     */
-    sections(table) {
-        if (table.tBodies.length > 0 && table.tBodies[0].rows[0] && (!table.tHead || !table.tFoot)) {
-            const length = table.tBodies[0].rows[0].cells.length;
-            let row;
-
-            if (!table.tHead) {
-                row = table.createTHead().insertRow();
-
-                for (let i = 0; i < length; i++) {
-                    row.appendChild(this.editor.createElement('th'));
-                }
+    initTable(node) {
+        if (node.tBodies.length > 0 && node.tBodies[0].rows[0] && (!node.tHead || !node.tFoot)) {
+            if (!node.tHead) {
+                this.createRow(node.createTHead(), node.tBodies[0].rows[0].cells.length);
             }
 
-            if (!table.tFoot) {
-                row = table.createTFoot().insertRow();
-
-                for (let i = 0; i < length; i++) {
-                    row.insertCell();
-                }
+            if (!node.tFoot) {
+                this.createRow(node.createTFoot(), node.tBodies[0].rows[0].cells.length);
             }
         }
     }
 
     /**
-     * Handles keyboard events
+     * Initializes table cell element
      *
      * @private
-     * @param {HTMLTableElement} table
+     * @param {HTMLTableCellElement} node
      */
-    keyboard(table) {
-        table.addEventListener('keydown', event => {
-            const cell = event.target;
-            const row = cell.parentElement;
-            const base = row.parentElement;
-            const keys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
+    initCell(node) {
+        node.addEventListener('keydown', this);
+    }
 
-            if (cell instanceof HTMLTableCellElement
-                && row instanceof HTMLTableRowElement
-                && (base instanceof HTMLTableElement || base instanceof HTMLTableSectionElement)
-                && (this.editor.isKey(event, keys, {alt: true}) || this.editor.isKey(event, keys, {alt: true, shift: true}))
-            ) {
-                const length = row.cells.length;
-                const rowLength = base.rows.length;
-                const rowIndex = base instanceof HTMLTableElement ? row.rowIndex : row.sectionRowIndex;
-                let index;
+    /**
+     * Handles key combinations for sorting
+     *
+     * @private
+     * @param {KeyboardEvent} event
+     * @param {HTMLTableCellElement} event.target
+     */
+    keydown(event) {
+        const cell = event.target;
+        const row = cell.parentElement;
+        const base = row.parentElement;
+        const table = base instanceof HTMLTableElement ? base : base.parentElement;
+        const keys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
 
-                if (event.shiftKey && (event.key === 'ArrowLeft' && cell.cellIndex > 0 || event.key === 'ArrowRight' && cell.cellIndex < length - 1)) {
-                    index = cell.cellIndex + (event.key === 'ArrowLeft' ? -1 : 1);
-                    Array.from(table.rows).forEach(item => item.deleteCell(index));
-                } else if (event.shiftKey && (event.key === 'ArrowUp' && rowIndex > 0 || event.key === 'ArrowDown' && rowIndex < rowLength - 1)) {
-                    index = rowIndex + (event.key === 'ArrowUp' ? -1 : 1);
-                    base.deleteRow(index)
-                } else if (!event.shiftKey && (event.key === 'ArrowLeft' || event.key === 'ArrowRight')) {
-                    index = cell.cellIndex + (event.key === 'ArrowLeft' ? 0 : 1);
-                    Array.from(table.rows).forEach(item => {
-                        if (item.parentElement.localName === 'thead') {
-                            item.insertBefore(this.editor.createElement('th'), item.cells[index]);
-                        } else {
-                            item.insertCell(index);
-                        }
-                    });
-                } else if (!event.shiftKey && (event.key === 'ArrowUp' || event.key === 'ArrowDown')) {
-                    index = rowIndex + (event.key === 'ArrowUp' ? 0 : 1);
-                    const r = base.insertRow(index);
+        if (cell instanceof HTMLTableCellElement
+            && row instanceof HTMLTableRowElement
+            && (base instanceof HTMLTableElement || base instanceof HTMLTableSectionElement)
+            && table instanceof HTMLTableElement
+            && (this.editor.isKey(event, keys, {alt: true}) || this.editor.isKey(event, keys, {alt: true, shift: true}))
+        ) {
+            const length = row.cells.length;
+            const rowLength = base.rows.length;
+            const rowIndex = base instanceof HTMLTableElement ? row.rowIndex : row.sectionRowIndex;
 
-                    for (let i = 0; i < length; i++) {
-                        if (r.parentElement.localName === 'thead') {
-                            r.appendChild(this.editor.createElement('th'));
-                        } else {
-                            r.insertCell();
-                        }
-                    }
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (event.shiftKey) {
+                if (event.key === 'ArrowLeft' && cell.cellIndex > 0) {
+                    Array.from(table.rows).forEach(item => item.deleteCell(cell.cellIndex - 1));
+                } else if (event.key === 'ArrowRight' && cell.cellIndex < length - 1) {
+                    Array.from(table.rows).forEach(item => item.deleteCell(cell.cellIndex + 1));
+                } else if (event.key === 'ArrowUp' && rowIndex > 0) {
+                    base.deleteRow(rowIndex - 1)
+                } else if (event.key === 'ArrowDown' && rowIndex < rowLength - 1) {
+                    base.deleteRow(rowIndex + 1)
                 }
-
-                event.preventDefault();
-                event.stopPropagation();
+            } else if (event.key === 'ArrowLeft') {
+                Array.from(table.rows).forEach(item => this.createCell(item, item.cells[cell.cellIndex]));
+            } else if (event.key === 'ArrowRight') {
+                console.log(table, table.rows);
+                Array.from(table.rows).forEach(item => this.createCell(item, item.cells[cell.cellIndex + 1]));
+            } else if (event.key === 'ArrowUp') {
+                this.createRow(base, length, rowIndex);
+            } else if (event.key === 'ArrowDown') {
+                this.createRow(base, length, rowIndex + 1);
             }
-        });
+        }
+    }
+
+    /**
+     * Creates table row
+     *
+     * @private
+     * @param {HTMLTableElement|HTMLTableSectionElement} node
+     * @param {Number} length
+     * @param {Number} [index = 0]
+     */
+    createRow(node, length, index = 0) {
+        const row = node.insertRow(index);
+
+        for (let i = 0; i < length; i++) {
+            this.createCell(row);
+        }
+    }
+
+    /**
+     * Creates table cell
+     *
+     * @private
+     * @param {HTMLTableRowElement} node
+     * @param {?HTMLTableCellElement} [ref = null]
+     */
+    createCell(node, ref = null) {
+        const name = node.parentElement.localName === 'thead' ? 'th' : 'td';
+        node.insertBefore(this.editor.createElement(name), ref);
     }
 }
