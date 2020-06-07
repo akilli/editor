0ed7f4c3fa527278892b305e0844980c9f58ada2
Ayhan Akilli <info@ayhan.biz>
2020-06-07 16:27:38 +0200

    Avoid multiple registrations of the same listeners due to arrow function usage

--- src/base/ToolbarObserver.js
+++ src/base/ToolbarObserver.js
@@ -10,59 +10,69 @@ export default class ToolbarObserver extends Observer {
     observe(records) {
         records.forEach(record => record.addedNodes.forEach(node => {
             if (node instanceof HTMLElement) {
-                if (node.hasAttribute('data-command')) {
-                    this.click(node);
+                if (this.editor.toolbar === node.parentElement || node.hasAttribute('data-command')) {
+                    this.init(node);
                 }
 
-                if (this.editor.toolbar === node.parentElement) {
-                    node.tabIndex = -1;
-                    this.keyboard(node);
-                }
-
-                node.querySelectorAll('[data-command]').forEach(item => this.click(item));
+                node.querySelectorAll('[data-command]').forEach(item => this.init(item));
             }
         }));
+    }
+
+    /**
+     * Initializes toolbar item
+     *
+     * @private
+     * @param {HTMLElement} node
+     */
+    init(node) {
+        node.addEventListener('click', this);
+
+        if (node.parentElement === this.editor.toolbar) {
+            node.tabIndex = -1;
+            node.addEventListener('keydown', this);
+        }
     }
 
     /**
      * Handles click events
      *
      * @private
-     * @param {HTMLElement} node
+     * @param {MouseEvent} event
+     * @param {HTMLElement} event.target
      */
-    click(node) {
-        node.addEventListener('click', () => this.editor.commands.execute(node.getAttribute('data-command')));
+    click(event) {
+        this.editor.commands.execute(event.target.getAttribute('data-command'));
     }
 
     /**
-     * Handles keyboard events
+     * Handles key combinations for navigation
      *
      * @private
-     * @param {HTMLElement} node
+     * @param {KeyboardEvent} event
+     * @param {HTMLElement} event.target
      */
-    keyboard(node) {
-        node.addEventListener('keydown', event => {
-            if (this.editor.isKey(event, ['ArrowLeft', 'ArrowRight', 'Home', 'End'])) {
-                const prev = node.previousElementSibling;
-                const next = node.nextElementSibling;
-                const first = node.parentElement.firstElementChild;
-                const last = node.parentElement.lastElementChild;
-                const isFirst = node === first;
-                const isLast = node === last;
+    keydown(event) {
+        if (this.editor.isKey(event, ['ArrowLeft', 'ArrowRight', 'Home', 'End'])) {
+            const prev = event.target.previousElementSibling;
+            const next = event.target.nextElementSibling;
+            const first = event.target.parentElement.firstElementChild;
+            const last = event.target.parentElement.lastElementChild;
+            const isFirst = event.target === first;
+            const isLast = event.target === last;
 
-                if (event.key === 'ArrowLeft' && !isFirst) {
-                    prev.focus();
-                } else if (event.key === 'ArrowRight' && !isLast) {
-                    next.focus();
-                } else if (event.key === 'Home' || event.key === 'ArrowRight' && isLast) {
-                    first.focus();
-                } else if (event.key === 'End' || event.key === 'ArrowLeft' && isFirst) {
-                    last.focus();
-                }
-
-                event.preventDefault();
-                event.stopPropagation();
+            if (event.key === 'ArrowLeft' && !isFirst) {
+                prev.focus();
+            } else if (event.key === 'ArrowRight' && !isLast) {
+                next.focus();
+            } else if (event.key === 'Home' || event.key === 'ArrowRight' && isLast) {
+                first.focus();
+            } else if (event.key === 'End' || event.key === 'ArrowLeft' && isFirst) {
+                last.focus();
             }
-        });
+
+            event.preventDefault();
+            event.stopPropagation();
+        }
     }
 }
