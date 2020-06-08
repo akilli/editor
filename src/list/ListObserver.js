451739cadac702467254395a8e2696bc536ff687
Ayhan Akilli <info@ayhan.biz>
2020-06-08 22:11:25 +0200

    Extend ListObserver

--- src/list/ListObserver.js
+++ src/list/ListObserver.js
@@ -7,13 +7,26 @@ export default class ListObserver extends Observer {
     /**
      * @inheritDoc
      */
+    constructor(editor) {
+        super(editor);
+        this.editor.content.addEventListener('sethtml', this);
+    }
+
+    /**
+     * @inheritDoc
+     */
     observe(records) {
         records.forEach(record => {
             record.addedNodes.forEach(node => {
-                if (node instanceof HTMLOListElement || node instanceof HTMLUListElement) {
-                    this.init(node);
-                } else if (node instanceof HTMLElement) {
-                    node.querySelectorAll('ol, ul').forEach(item => this.init(item));
+                if (node instanceof HTMLElement) {
+                    if (node instanceof HTMLOListElement || node instanceof HTMLUListElement) {
+                        this.initList(node);
+                    } else if (node instanceof HTMLLIElement) {
+                        this.initItem(node);
+                    }
+
+                    node.querySelectorAll('ol, ul').forEach(item => this.initList(item));
+                    node.querySelectorAll('li').forEach(item => this.initItem(item));
                 }
             });
 
@@ -27,14 +40,38 @@ export default class ListObserver extends Observer {
     }
 
     /**
-     * Initializes orderedlist element
+     * Initializes list element
      *
      * @private
      * @param {HTMLOListElement|HTMLUListElement} node
      */
-    init(node) {
+    initList(node) {
         if (node.children.length === 0) {
             node.appendChild(this.editor.createElement('li'));
+        }
+    }
+
+    /**
+     * Initializes list item element
+     *
+     * @private
+     * @param {HTMLLIElement} node
+     */
+    initItem(node) {
+        if (!(node.parentElement instanceof HTMLOListElement) && !(node.parentElement instanceof HTMLUListElement)) {
+            this.editor.wrapElement(node, 'ul');
+        }
+    }
+
+    /**
+     * Initializes list item elements when editor html is set
+     *
+     * @private
+     * @param {CustomEvent} event
+     */
+    sethtml(event) {
+        if (event.detail instanceof HTMLElement) {
+            event.detail.querySelectorAll('li').forEach(item => this.initItem(item));
         }
     }
 }
