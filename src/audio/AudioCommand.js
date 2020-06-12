d0047c086b74a5312f7175b7f83eac0e6cb93199
Ayhan Akilli <info@ayhan.biz>
2020-06-12 13:53:00 +0200

    Ignore caption, focus on media element

--- src/audio/AudioCommand.js
+++ src/audio/AudioCommand.js
@@ -18,18 +18,16 @@ export default class AudioCommand extends Command {
      *
      * @protected
      * @param {String} src
-     * @param {String} [caption = '']
      * @param {String} [width = '']
      * @param {String} [height = '']
      */
-    _insert({src, caption = '', width = '', height = ''} = {}) {
+    _insert({src, width = '', height = ''} = {}) {
         if (!src) {
             throw 'Invalid argument';
         }
 
         const figure = this.editor.createElement('figure', {attributes: {class: 'audio'}});
         figure.appendChild(this.editor.createElement('audio', {attributes: {src: this.editor.url(src), width, height}}));
-        figure.appendChild(this.editor.createElement('figcaption', {html: caption}));
         this.editor.insert(figure);
     }
 }
--- src/image/ImageCommand.js
+++ src/image/ImageCommand.js
@@ -18,19 +18,17 @@ export default class ImageCommand extends Command {
      *
      * @protected
      * @param {String} src
-     * @param {String} [caption = '']
      * @param {String} [width = '']
      * @param {String} [height = '']
      * @param {String} [alt = '']
      */
-    _insert({src, caption = '', width = '', height = '', alt = ''} = {}) {
+    _insert({src, width = '', height = '', alt = ''} = {}) {
         if (!src) {
             throw 'Invalid argument';
         }
 
         const figure = this.editor.createElement('figure', {attributes: {class: 'image'}});
         figure.appendChild(this.editor.createElement('img', {attributes: {src: this.editor.url(src), width, height, alt}}));
-        figure.appendChild(this.editor.createElement('figcaption', {html: caption}));
         this.editor.insert(figure);
     }
 }
