a7134c0ef8e39867c24990442b074acfc393f722
Ayhan Akilli <info@ayhan.biz>
2020-07-12 10:14:19 +0200

    Remove bundle.js

--- bundle.js
+++
@@ -1,6 +0,0 @@
-import Editor from './src/editor/Editor.js';
-import './src/editor/editor.css';
-import './src/editor/editor.woff2';
-
-// noinspection JSUnusedGlobalSymbols
-export default Editor;
--- rollup.config.js
+++ rollup.config.js
@@ -3,27 +3,39 @@ import esbuild from 'rollup-plugin-esbuild';
 import url from '@rollup/plugin-url';
 
 // noinspection JSUnusedGlobalSymbols
-export default {
-    input: 'bundle.js',
-    output: {
-        file: 'dist/editor.js',
-        format: 'esm',
-        preferConst: true,
+export default [
+    {
+        input: 'src/editor/Editor.js',
+        output: {
+            file: 'dist/editor.js',
+            format: 'esm',
+            preferConst: true,
+        },
+        plugins: [
+            esbuild({
+                minify: true,
+                target: 'es2020',
+            }),
+        ],
     },
-    plugins: [
-        css({
-            minified: 'dist/editor.css',
-            raw: false,
-        }),
-        url({
-            destDir: 'dist',
-            fileName: '[name][extname]',
-            include: ['**/*.woff2'],
-            limit: 0,
-        }),
-        esbuild({
-            minify: true,
-            target: 'es2020',
-        }),
-    ]
-};
+    {
+        input: 'src/editor/editor.css',
+        plugins: [
+            css({
+                minified: 'dist/editor.css',
+                raw: false,
+            }),
+        ],
+    },
+    {
+        input: 'src/editor/editor.woff2',
+        plugins: [
+            url({
+                destDir: 'dist',
+                fileName: '[name][extname]',
+                include: ['**/*.woff2'],
+                limit: 0,
+            }),
+        ],
+    },
+];
