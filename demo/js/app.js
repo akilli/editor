9c598d4f1a5ff72a8b7895951c16a6521311d711
Ayhan Akilli <info@ayhan.biz>
2021-11-06 20:10:17 +0100

    Cleanup

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,51 +1,48 @@
-'use strict';
+const config = {
+    audio: {
+        browser: 'browser/audio.html',
+    },
+    block: {
+        api: 'api/{id}.html',
+        browser: 'browser/block.html',
+        css: 'css/base.css,css/app.css',
+    },
+    iframe: {
+        browser: 'browser/iframe.html',
+    },
+    image: {
+        browser: 'browser/image.html',
+    },
+    video: {
+        browser: 'browser/video.html',
+    },
+};
 
-(function (document, window, console) {
-    function editorPath() {
-        return window.location.pathname.endsWith('src.html') ? '../../src/editor/Editor.js' : '../../dist/editor.js';
-    }
+document.addEventListener('DOMContentLoaded', async () => {
+    const rte = document.getElementById('rte');
+    const { default: Editor } = await import(editorPath());
+    const editor = Editor.create(rte, config);
+    console.log(editor);
 
-    document.addEventListener('DOMContentLoaded', async () => {
-        const config = {
-            audio: {
-                browser: 'browser/audio.html',
-            },
-            block: {
-                api: 'api/{id}.html',
-                browser: 'browser/block.html',
-                css: 'css/base.css,css/app.css',
-            },
-            iframe: {
-                browser: 'browser/iframe.html',
-            },
-            image: {
-                browser: 'browser/image.html',
-            },
-            video: {
-                browser: 'browser/video.html',
-            },
-        };
-        const rte = document.getElementById('rte');
-        const { default: Editor } = await import(editorPath());
-        const editor = Editor.create(rte, config);
-        console.log(editor);
-
-        document.getElementById('clear').addEventListener('click', () => {
-            editor.setHtml('');
-            window.scrollTo(0, 0);
-        });
-
-        const save = document.getElementById('save');
-        save.textContent = rte.hidden ? 'Save' : 'Edit';
-        save.addEventListener('click', () => {
-            if (rte.hidden) {
-                editor.save();
-                editor.destroy();
-                save.textContent = 'Edit';
-            } else {
-                editor.load();
-                save.textContent = 'Save';
-            }
-        });
+    document.getElementById('clear').addEventListener('click', () => {
+        editor.setHtml('');
+        window.scrollTo(0, 0);
     });
-})(document, window, console);
+
+    const save = document.getElementById('save');
+    save.textContent = rte.hidden ? 'Save' : 'Edit';
+    save.addEventListener('click', () => {
+        if (rte.hidden) {
+            editor.save();
+            editor.destroy();
+            save.textContent = 'Edit';
+        } else {
+            editor.load();
+            save.textContent = 'Save';
+        }
+    });
+});
+
+function editorPath() {
+    return window.location.pathname.endsWith('src.html') ? '../../src/editor/Editor.js' : '../../dist/editor.js';
+}
