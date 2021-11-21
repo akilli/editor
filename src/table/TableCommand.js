aa06369cf117c05b6716703803b7ac08f83b6aa0
Ayhan Akilli <info@ayhan.biz>
2021-11-21 12:19:03 +0100

    Remove TableDom again

--- src/table/TableCommand.js
+++ src/table/TableCommand.js
@@ -1,6 +1,5 @@
 import Command from '../base/Command.js';
 import Table from './Table.js';
-import TableDom from './TableDom.js';
 import { TagName } from '../base/enum.js';
 
 /**
@@ -8,29 +7,12 @@ import { TagName } from '../base/enum.js';
  */
 export default class TableCommand extends Command {
     /**
-     * Table DOM manager
-     *
-     * @type {TableDom}
-     */
-    #tableDom;
-
-    /**
-     * Allows read access to table DOM manager
-     *
-     * @return {TableDom}
-     */
-    get tableDom() {
-        return this.#tableDom;
-    }
-
-    /**
      * Initializes a new table command
      *
      * @param {Editor} editor
      */
     constructor(editor) {
         super(editor, Table.name, TagName.TABLE);
-        this.#tableDom = new TableDom(editor.dom);
     }
 
     /**
@@ -42,7 +24,22 @@ export default class TableCommand extends Command {
      */
     insert({ rows, cols } = {}) {
         const figure = this.editor.dom.createElement(TagName.FIGURE, { attributes: { class: Table.name } });
-        figure.appendChild(this.tableDom.createTable(parseInt(rows, 10), parseInt(cols, 10)));
+        const table = this.editor.dom.createElement(TagName.TABLE);
+        const tbody = this.editor.dom.createElement(TagName.TBODY);
+        const r = parseInt(rows, 10) || 1;
+        const c = parseInt(cols, 10) || 1;
+
+        for (let i = 0; i < r; i++) {
+            const tr = this.editor.dom.createElement(TagName.TR);
+            tbody.appendChild(tr);
+
+            for (let j = 0; j < c; ++j) {
+                tr.appendChild(this.editor.dom.createElement(TagName.TD));
+            }
+        }
+
+        table.appendChild(tbody);
+        figure.appendChild(table);
         this.editor.dom.insert(figure);
     }
 }
