ee5ab32f6bdf9d9579532d24dbcbe85195ae4872
Ayhan Akilli <info@ayhan.biz>
2021-11-28 11:57:04 +0100

    Cleanup

--- src/table/Table.js
+++ src/table/Table.js
@@ -39,44 +39,50 @@ export default class Table extends Plugin {
             deletable: true,
             navigable: true,
         });
-        this._tag({
-            name: TagName.THEAD,
-            group: TagGroup.TABLESECTION,
-            children: [TagGroup.TABLEROW],
-        });
-        this._tag({
-            name: TagName.TBODY,
-            group: TagGroup.TABLESECTION,
-            children: [TagGroup.TABLEROW],
-        });
-        this._tag({
-            name: TagName.TFOOT,
-            group: TagGroup.TABLESECTION,
-            children: [TagGroup.TABLEROW],
-        });
+        this._tag(this.#section(TagName.THEAD));
+        this._tag(this.#section(TagName.TBODY));
+        this._tag(this.#section(TagName.TFOOT));
         this._tag({
             name: TagName.TR,
             group: TagGroup.TABLEROW,
             children: [TagGroup.TABLECELL],
         });
-        this._tag({
-            name: TagName.TH,
-            group: TagGroup.TABLECELL,
-            children: [TagGroup.BREAK, TagGroup.FORMAT],
-            editable: true,
-            empty: true,
-        });
-        this._tag({
-            name: TagName.TD,
-            group: TagGroup.TABLECELL,
-            children: [TagGroup.BREAK, TagGroup.FORMAT],
-            editable: true,
-            empty: true,
-        });
+        this._tag(this.#cell(TagName.TH));
+        this._tag(this.#cell(TagName.TD));
         new TableListener(this.editor);
         this.editor.filters.add(new TableFilter(this.editor));
         this.editor.dialogs.set(new TableDialog(this.editor));
         this.editor.commands.set(new TableCommand(this.editor));
         this._toolbar(this._('Table'));
+    }
+
+    /**
+     * Returns table section tag configuration
+     *
+     * @param {string} name
+     * @return {Object.<string, any>}
+     */
+    #section(name) {
+        return {
+            name,
+            group: TagGroup.TABLESECTION,
+            children: [TagGroup.TABLEROW],
+        };
+    }
+
+    /**
+     * Returns table cell tag configuration
+     *
+     * @param {string} name
+     * @return {Object.<string, any>}
+     */
+    #cell(name) {
+        return {
+            name,
+            group: TagGroup.TABLECELL,
+            children: [TagGroup.BREAK, TagGroup.FORMAT],
+            editable: true,
+            empty: true,
+        };
     }
 }
