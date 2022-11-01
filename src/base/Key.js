682ffa3dfc4e720c5e023d7a5ca658c03dcea706
Ayhan Akilli <info@ayhan.biz>
2022-11-01 11:45:58 +0100

    Extract Key enum part 2

--- src/base/Key.js
+++ src/base/Key.js
@@ -1,42 +1,156 @@
 /**
  * Keys
- *
- * @enum {string}
  */
-export default Object.freeze({
-    A: 'a',
-    B: 'b',
-    BACKSPACE: 'Backspace',
-    C: 'c',
-    D: 'd',
-    DEL: 'Delete',
-    DOWN: 'ArrowDown',
-    E: 'e',
-    END: 'End',
-    ENTER: 'Enter',
-    ESC: 'Escape',
-    F: 'f',
-    G: 'g',
-    HOME: 'Home',
-    I: 'i',
-    J: 'j',
-    K: 'k',
-    L: 'l',
-    LEFT: 'ArrowLeft',
-    M: 'm',
-    O: 'o',
-    Q: 'q',
-    R: 'r',
-    RIGHT: 'ArrowRight',
-    S: 's',
-    SPACE: ' ',
-    T: 't',
-    TAB: 'Tab',
-    U: 'u',
-    UP: 'ArrowUp',
-    V: 'v',
-    W: 'w',
-    X: 'x',
-    Y: 'y',
-    Z: 'z',
-});
+export default class Key {
+    static get A() {
+        return 'a';
+    }
+
+    static get B() {
+        return 'b';
+    }
+
+    static get BACKSPACE() {
+        return 'Backspace';
+    }
+
+    static get C() {
+        return 'c';
+    }
+
+    static get D() {
+        return 'd';
+    }
+
+    static get DEL() {
+        return 'Delete';
+    }
+
+    static get DOWN() {
+        return 'ArrowDown';
+    }
+
+    static get E() {
+        return 'e';
+    }
+
+    static get END() {
+        return 'End';
+    }
+
+    static get ENTER() {
+        return 'Enter';
+    }
+
+    static get ESC() {
+        return 'Escape';
+    }
+
+    static get F() {
+        return 'f';
+    }
+
+    static get G() {
+        return 'g';
+    }
+
+    static get H() {
+        return 'h';
+    }
+
+    static get HOME() {
+        return 'Home';
+    }
+
+    static get I() {
+        return 'i';
+    }
+
+    static get J() {
+        return 'j';
+    }
+
+    static get K() {
+        return 'k';
+    }
+
+    static get L() {
+        return 'l';
+    }
+
+    static get LEFT() {
+        return 'ArrowLeft';
+    }
+
+    static get M() {
+        return 'm';
+    }
+
+    static get N() {
+        return 'n';
+    }
+
+    static get O() {
+        return 'o';
+    }
+
+    static get P() {
+        return 'p';
+    }
+
+    static get Q() {
+        return 'q';
+    }
+
+    static get R() {
+        return 'r';
+    }
+
+    static get RIGHT() {
+        return 'ArrowRight';
+    }
+
+    static get S() {
+        return 's';
+    }
+
+    static get SPACE() {
+        return ' ';
+    }
+
+    static get T() {
+        return 't';
+    }
+
+    static get TAB() {
+        return 'Tab';
+    }
+
+    static get U() {
+        return 'u';
+    }
+
+    static get UP() {
+        return 'ArrowUp';
+    }
+
+    static get V() {
+        return 'v';
+    }
+
+    static get W() {
+        return 'w';
+    }
+
+    static get X() {
+        return 'x';
+    }
+
+    static get Y() {
+        return 'y';
+    }
+
+    static get Z() {
+        return 'z';
+    }
+}
