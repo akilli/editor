d425e5a2467b855aa9a62f461a257bdb86106588
Ayhan Akilli <info@ayhan.biz>
2023-05-01 11:34:26 +0200

    Reorder code

--- src/base/Key.js
+++ src/base/Key.js
@@ -1,23 +1,4 @@
 /**
- * Indicates if keyboard event was triggered for given key combination
- *
- * @param {KeyboardEvent} event
- * @param {string|string[]} key
- * @param {boolean} alt
- * @param {boolean} ctrl
- * @param {boolean} shift
- * @return {boolean}
- */
-export function isKey(event, key, { alt = false, ctrl = false, shift = false } = {}) {
-    return (
-        ((Array.isArray(key) && key.includes(event.key)) || event.key === key) &&
-        event.altKey === alt &&
-        event.ctrlKey === ctrl &&
-        event.shiftKey === shift
-    );
-}
-
-/**
  * @enum {string}
  */
 export default Object.freeze({
@@ -56,3 +37,22 @@ export default Object.freeze({
     Y: 'y',
     Z: 'z',
 });
+
+/**
+ * Indicates if keyboard event was triggered for given key combination
+ *
+ * @param {KeyboardEvent} event
+ * @param {string|string[]} key
+ * @param {boolean} alt
+ * @param {boolean} ctrl
+ * @param {boolean} shift
+ * @return {boolean}
+ */
+export function isKey(event, key, { alt = false, ctrl = false, shift = false } = {}) {
+    return (
+        ((Array.isArray(key) && key.includes(event.key)) || event.key === key) &&
+        event.altKey === alt &&
+        event.ctrlKey === ctrl &&
+        event.shiftKey === shift
+    );
+}
